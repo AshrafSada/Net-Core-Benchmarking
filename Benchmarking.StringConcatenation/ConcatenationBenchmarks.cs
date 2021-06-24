e250db27dee91eb3b544a8beec36a96ac64e623d
Ashraf Sada <ashraf.sada@outlook.com>
2021-06-24 17:37:47 +0300

    Added string interpolation and string format benchmarks

--- Benchmarking.StringConcatenation/ConcatenationBenchmarks.cs
+++ Benchmarking.StringConcatenation/ConcatenationBenchmarks.cs
@@ -3,16 +3,17 @@ using BenchmarkDotNet.Order;
 
 namespace Benchmarking.StringConcatenation
 {
-    [MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
-    public class ConcatenationBenchmarks
-    {
-        [Benchmark]
-        public string StringsAddedByPlus() => StringConcatentions.AddStrings();
+   [MemoryDiagnoser, Orderer(SummaryOrderPolicy.FastestToSlowest)]
+   public class ConcatenationBenchmarks
+   {
+      [Benchmark] public string StringsAddedByPlus() => StringConcatentions.AddStrings();
 
-        [Benchmark]
-        public string StringsConcatenated() => StringConcatentions.ConcatenateStrings();
+      [Benchmark] public string StringsConcatenated() => StringConcatentions.ConcatenateStrings();
 
-        [Benchmark]
-        public string StringsAggregatedByStringBuilder() => StringConcatentions.ConcatStringBuilderStrings();
-    }
+      [Benchmark] public string StringsAggregatedByStringBuilder() => StringConcatentions.ConcatStringBuilderStrings();
+
+      [Benchmark] public string StringsAggregatedByStringFormat() => StringConcatentions.ConcatStringWithStringFormat();
+
+      [Benchmark] public string StringsAggregatedByStringInterpolation() => StringConcatentions.ConcatStringWithStringInterpolation();
+   }
 }
--- Benchmarking.StringConcatenation/StringConcatentions.cs
+++ Benchmarking.StringConcatenation/StringConcatentions.cs
@@ -3,71 +3,107 @@ using System.Text;
 
 namespace Benchmarking.StringConcatenation
 {
-    public class StringConcatentions
-    {
-        public static string AddStrings()
-        {
-            string s1 = new String('A', 100);
-            string s2 = new String('A', 100);
-            string s3 = new String('A', 100);
-            string s4 = new String('A', 100);
-            string s5 = new String('A', 100);
-            string s6 = new String('A', 100);
-            string s7 = new String('A', 100);
-            string s8 = new String('A', 100);
-            string s9 = new String('A', 100);
-            string s10 = new String('A', 100);
+   public class StringConcatentions
+   {
+      public static string AddStrings()
+      {
+         string s1 = new String('A', 100);
+         string s2 = new String('A', 100);
+         string s3 = new String('A', 100);
+         string s4 = new String('A', 100);
+         string s5 = new String('A', 100);
+         string s6 = new String('A', 100);
+         string s7 = new String('A', 100);
+         string s8 = new String('A', 100);
+         string s9 = new String('A', 100);
+         string s10 = new String('A', 100);
 
-            string sx = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10;
+         string sx = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10;
 
-            return sx;
-        }
+         return sx;
+      }
 
-        public static string ConcatenateStrings()
-        {
-            string s1 = new String('A', 100);
-            string s2 = new String('A', 100);
-            string s3 = new String('A', 100);
-            string s4 = new String('A', 100);
-            string s5 = new String('A', 100);
-            string s6 = new String('A', 100);
-            string s7 = new String('A', 100);
-            string s8 = new String('A', 100);
-            string s9 = new String('A', 100);
-            string s10 = new String('A', 100);
+      public static string ConcatenateStrings()
+      {
+         string s1 = new String('A', 100);
+         string s2 = new String('A', 100);
+         string s3 = new String('A', 100);
+         string s4 = new String('A', 100);
+         string s5 = new String('A', 100);
+         string s6 = new String('A', 100);
+         string s7 = new String('A', 100);
+         string s8 = new String('A', 100);
+         string s9 = new String('A', 100);
+         string s10 = new String('A', 100);
 
-            string xy = String.Concat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
+         string xy = String.Concat(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
 
-            return xy;
-        }
+         return xy;
+      }
 
-        public static string ConcatStringBuilderStrings()
-        {
-            string s1 = new String('A', 100);
-            string s2 = new String('A', 100);
-            string s3 = new String('A', 100);
-            string s4 = new String('A', 100);
-            string s5 = new String('A', 100);
-            string s6 = new String('A', 100);
-            string s7 = new String('A', 100);
-            string s8 = new String('A', 100);
-            string s9 = new String('A', 100);
-            string s10 = new String('A', 100);
+      public static string ConcatStringBuilderStrings()
+      {
+         string s1 = new String('A', 100);
+         string s2 = new String('A', 100);
+         string s3 = new String('A', 100);
+         string s4 = new String('A', 100);
+         string s5 = new String('A', 100);
+         string s6 = new String('A', 100);
+         string s7 = new String('A', 100);
+         string s8 = new String('A', 100);
+         string s9 = new String('A', 100);
+         string s10 = new String('A', 100);
 
-            StringBuilder sb = new StringBuilder();
+         StringBuilder sb = new StringBuilder();
 
-            _ = sb.Append(s1);
-            _ = sb.Append(s2);
-            _ = sb.Append(s3);
-            _ = sb.Append(s4);
-            _ = sb.Append(s5);
-            _ = sb.Append(s6);
-            _ = sb.Append(s7);
-            _ = sb.Append(s8);
-            _ = sb.Append(s9);
-            _ = sb.Append(s10);
+         _ = sb.Append(s1);
+         _ = sb.Append(s2);
+         _ = sb.Append(s3);
+         _ = sb.Append(s4);
+         _ = sb.Append(s5);
+         _ = sb.Append(s6);
+         _ = sb.Append(s7);
+         _ = sb.Append(s8);
+         _ = sb.Append(s9);
+         _ = sb.Append(s10);
 
-            return sb.ToString();
-        }
-    }
+         return sb.ToString();
+      }
+
+      public static string ConcatStringWithStringFormat()
+      {
+         string s1 = new String('A', 100);
+         string s2 = new String('A', 100);
+         string s3 = new String('A', 100);
+         string s4 = new String('A', 100);
+         string s5 = new String('A', 100);
+         string s6 = new String('A', 100);
+         string s7 = new String('A', 100);
+         string s8 = new String('A', 100);
+         string s9 = new String('A', 100);
+         string s10 = new String('A', 100);
+
+         string sf = String.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}", s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
+
+         return sf;
+      }
+
+      public static string ConcatStringWithStringInterpolation()
+      {
+         string s1 = new String('A', 100);
+         string s2 = new String('A', 100);
+         string s3 = new String('A', 100);
+         string s4 = new String('A', 100);
+         string s5 = new String('A', 100);
+         string s6 = new String('A', 100);
+         string s7 = new String('A', 100);
+         string s8 = new String('A', 100);
+         string s9 = new String('A', 100);
+         string s10 = new String('A', 100);
+
+         string si = $"{s1}{s2}{s3}{s4}{s5}{s6}{s7}{s8}{s9}{s10}";
+
+         return si;
+      }
+   }
 }
